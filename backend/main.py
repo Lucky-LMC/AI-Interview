4789a846d0666d989367cdb82861bf88beb4c6cb
Lucky-LMC <mengchangliu5@gmail.com>
2026-01-16 19:30:41 +0800

    更新代码

--- backend/main.py
+++ backend/main.py
@@ -56,8 +56,6 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         "main:app",
-        # host="172.18.174.107",
-        # python -m http.server 8080
         host="0.0.0.0",
         port=8000,
         reload=True
